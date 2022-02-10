b27ffa307ba267498e7c42acde4032af67428560
hojin <hojin9car@gmail.com>
2022-02-11 02:19:56 +0900

    third commit

--- sparta/urls.py
+++ sparta/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
+from typing import Dict
+from django.http import HttpRequest
 
 api = NinjaAPI()
 
 
 @api.get("/add")
-def add(request, a: int, b: int):
+def add(request: HttpRequest, a: int, b: int) -> Dict[str, int]:
 
     return {"result": a + b}
 
